fb16792b616b35ddd4a248ac8ca4ba1da817d080
Alina-muk <118119073+Alina-muk@users.noreply.github.com>
2023-05-15 20:22:06 +0500

    задание 6.1.js

--- задание 6.1.js
+++ задание 6.1.js
@@ -30,24 +30,3 @@ function coountEvenOddElements () {
     console.log("Odd - " + result[1]);
     console.log("Other - " + result[2]);
     console.log("Zero - " + result[3]);
-
-
-
-
-
-
-for (let i = 0; i < arr.length; i++){
-    if (typeof(arr[i]) != 'number') {
-      elementOther++;
-    } else {
-        if  (arr[i] === 0) {
-          elementZero++;
-        } else {
-            if (arr[i] % 2 === 0) {
-              elementEven++;
-            } else {
-               elementOdd++;
-        }
-    }
-  }
-}
